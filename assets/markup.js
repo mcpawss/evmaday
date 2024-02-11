0dca8c72144c1171e15fc9504df5194c5997b017
mcpawss <158771577+mcpawss@users.noreply.github.com>
2024-02-11 13:04:14 +0000

    Add files via upload

---
+++ assets/markup.js
@@ -0,0 +1,72 @@
+
+export const a = `<html lang="en">
+<head>
+  <title>Minimalist Frame - A</title>
+  <meta property="fc:frame" content="vNext" />
+  <meta
+    property="fc:frame:image"
+    content="https://f4.bcbits.com/img/a0600385591_16.jpg" 
+  />
+</head>
+<body>
+  <h1>Minimalist Frame - A</h1>
+  <img id="randomImage" src="" alt="Random Image">
+</body>
+</html>`;
+  
+export const b = `<html lang="en">
+<head>
+  <title>Minimalist Frame - B</title>
+  <meta property="fc:frame" content="vNext" />
+  <meta
+    property="fc:frame:image"
+    content="https://minimalist-frame.vercel.app/img/b.gif"
+  />
+  <meta property="og:image" content="https://minimalist-frame.vercel.app/img/b.png" />
+  <meta property="fc:frame:image:aspect_ratio" content="1:1" />
+  <meta property="fc:frame:button:1" content="Go to A" />
+  <meta property="fc:frame:button:2" content="Go to C" />
+  <meta property="fc:frame:button:3" content="View source code" />
+  <meta property="fc:frame:button:3:action" content="link" />
+  <meta
+    property="fc:frame:button:3:target"
+    content="https://github.com/InsideTheSim/minimalist-frame"
+  />
+  <meta
+    property="fc:frame:post_url"
+    content="https://minimalist-frame.vercel.app/api/frame-b"
+  />
+</head>
+<body>
+  <h1>Minimalist Frame - B</h1>
+</body>
+</html>`
+
+export const c = `<html lang="en">
+<head>
+  <title>Minimalist Frame - C</title>
+  <meta property="fc:frame" content="vNext" />
+  <meta
+    property="fc:frame:image"
+    content="https://minimalist-frame.vercel.app/img/c.gif"
+  />
+  <meta property="og:image" content="https://minimalist-frame.vercel.app/img/c.png" />
+  <meta property="fc:frame:image:aspect_ratio" content="1:1" />
+  <meta property="fc:frame:button:1" content="Go to A" />
+  <meta property="fc:frame:button:2" content="Go to B" />
+  <meta property="fc:frame:button:3" content="View source code" />
+  <meta property="fc:frame:button:3:action" content="link" />
+  <meta
+    property="fc:frame:button:3:target"
+    content="https://github.com/InsideTheSim/minimalist-frame"
+  />
+  <meta
+    property="fc:frame:post_url"
+    content="https://minimalist-frame.vercel.app/api/frame-c"
+  />
+</head>
+<body>
+  <h1>Minimalist Frame - C</h1>
+</body>
+</html>
+`
---
+++ assets/router.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch';
+
+const getDailyImage = async () => {
+  const date = new Date();
+  const dayOfYear = Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 86400000);
+  const response = await fetch(`https://picsum.photos/seed/${dayOfYear}/1600/900`);
+  return response.url;
+}
+
+export default async function createRouter(target1, target2) {
+  const randomImage = await getDailyImage();
+  const html = target1.replace("https://f4.bcbits.com/img/a0600385591_16.jpg", randomImage);
+  return function (req, res) {
+    if (req.method === "POST") {
+      const data = req.body.untrustedData
+      if (!data) {
+        res.statusCode = 400
+        return res.send("Invalid request")
+      }
+      res.statusCode = 200
+      res.setHeader("Content-Type", "text/html")
+      if (data.buttonIndex === 1) {
+        return res.send(html)
+      }
+      if (data.buttonIndex === 2) {
+        return res.send(target2)
+      }
+    }
+    return res.status(400).end()
+  }
+}
